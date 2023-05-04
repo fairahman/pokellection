2ba1ac0da55efdfc6e4a70edad02be6659a31a84
Curtis.Lovrak <curtislovrak@gmail.com>
2023-05-04 13:47:51 -0400

    now log in is home screen

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,6 @@
 const { User } = require('../models/all_models.js');
 const bcrypt = require('bcryptjs');
+// const popup = require('.../src');
 
 
 const SALT_WORK_FACTOR = 10;
@@ -13,55 +14,111 @@ const userController = {};
 // using bcrypt we are hashing and storing the user to their MONGO database
 // we're setting res.locals.newUser to the returned data which is likely the new user
 
-userController.createUser = (req, res, next) => {
-    console.log("createUser middleware")
-    const { username } = req.body;
-    let { password } = req.body;
-    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-        bcrypt.hash(password, salt, (err, hash) => {
-            User.create({ username: username, password: hash })
-                .then(data => {
-                    res.locals.newUser = data;
-                    console.log("data", data)
-                    return next();
-                })
-                .catch(err => {
-                    const errObj = {
-                        log: 'Error occurred in user.create',
-                        status: 400,
-                        message: 'Error occurred'
-                    };
-                    return next(errObj);
-                })
-        })
-    })
-}
+// userController.createUser = (req, res, next) => {
+//   console.log("createUser middleware")
+//   const { username } = req.body; //check if username is already taken.
+//   let { password } = req.body;
+//   bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//       User.createfind({ username: username, password: hash })
+//         .then(data => {
+//           res.locals.newUser = data;
+//           // console.log("data", data)
+//           return next();
+//         })
+//         .catch(err => {
+//           const errObj = {
+//             log: 'Error occurred in user.create',
+//             status: 400,
+//              message: 'Error occurred'
+//           };
+//           return next(errObj);
+//         })
+//       })
+//     })
+// }
 
-// we are sending a findOne request with the username pulled from the request body to the MONGO database
+// // we are sending a findOne request with the username pulled from the request body to the MONGO database
 
 
 userController.getUser = (req, res, next) => {
-    //const { username } = req.body;
-    console.log("before user find one")
-    console.log("req body: ", req.body)
-    User.findOne({ username: req.body.username })/*, (err, result) => {*/
-        .then(async (results) => {
-            console.log("results", results)
-            const passwordMatch = await bcrypt.compare(req.body.password, results.password);
-            console.log('PASSWORD MATCH: ', passwordMatch);
-            res.locals.truthy = passwordMatch;
-            return next();
-        })
-        .catch((err) => {
-            console.log('THIS IS THE ERROR: ', err);
-            const errObj = {
-                log: 'AN ERROR IN THE usercontroller.getuser',
-                status: 400,
-                message: { err: "chill" },
-            };
-            return next(errObj);
-        });
+  //const { username } = req.body;
+  console.log("before user find one")
+  console.log("req body: ", req.body)
+  User.findOne({ username: req.body.username })/*, (err, result) => {*/
+    .then(async (results) => {
+      console.log("results", results)
+      const passwordMatch = await bcrypt.compare(req.body.password, results.password);
+      console.log('PASSWORD MATCH: ', passwordMatch);
+      res.locals.truthy = passwordMatch;
+      return next();
+    })
+    .catch((err) => {
+      console.log('THIS IS THE ERROR: ', err);
+      const errObj = {
+        log: 'AN ERROR IN THE usercontroller.getuser',
+        status: 400,
+        message: { err: "chill" },
+      };
+      return next(errObj);
+    });
 }
+
+userController.createUser = (req, res, next) => {
+  console.log("createUser middleware");
+  const { username } = req.body;
+  let { password } = req.body;
+
+  // Check if the username already exists
+  User.findOne({ username })
+    .then(existingUser => {
+      if (existingUser) {
+        // If the username already exists, route to a different handler
+        return userController.handleExistingUser(req, res, next);
+      } else {
+        // If the username doesn't exist, proceed with creating the new user
+        bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+          bcrypt.hash(password, salt, (err, hash) => {
+            User.create({ username, password: hash })
+              .then(data => {
+                res.locals.newUser = data;
+                return next();
+              })
+              .catch(err => {
+                const errObj = {
+                  log: 'Error occurred in user.create',
+                  status: 400,
+                  message: 'Error occurred'
+                };
+                return next(errObj);
+              });
+          });
+        });
+      }
+    })
+    .catch(err => {
+      const errObj = {
+        log: 'Error occurred in user.findOne',
+        status: 400,
+        message: 'Error occurred'
+      };
+      return next(errObj);
+    });
+}
+
+// Handler for when the user already exists
+userController.handleExistingUser = (req, res, next) => {
+  console.log("handleExistingUser middleware");
+  const { username } = req.body;
+  // alert("User Name Exists")
+  res.render( (fddf), { message: `Username ${username} already exists` });
+  const errObj = {
+    log: 'Username already exists',
+    status: 402,
+    message: `Username ${username} already exists`
+  };
+  return next(errObj);
+};
 
 
 module.exports = userController;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,63 @@
-import React from 'react';
+// import React from 'react';
 
-const Signup = () => {
-  return <h1>Signup Page</h1>;
-};
+// const Signup = () => {
+//   return <h1>Signup Page</h1>;
+// };
 
-export default Signup;
+// export default Signup;
+
+import React, { useState} from "react";
+import { Link } from "react-router-dom";
+// import { Input }
+import Select from "react-tailwindcss-select";
+// import Form from "react-tailwindcss-form";
+import MainContainer from "./MainContainer";
+
+export default function Signup(props) {
+
+
+	const[userName, setUserName] = useState()
+	const[passWord, setPassWord] = useState()
+	
+	function validateForm() {
+		return userName.length > 0 && passWord.length > 0
+	}
+	function handleSubmit(event) {
+		event.preventDefault()
+    fetch ("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({username: userName, password: passWord})
+    }).then(response => response.json()).then(response => console.log(response))
+
+    // setUserName(event.target.userName.value);
+    // setPassWord(event.target.passWord.value);
+    console.log("user name ",userName, "password ", passWord)
+	}
+	const handleChangeUserName = (value) => {
+		console.log("value:", value);
+		setUserName(value);
+	};
+  const handleChangePassWord = (value) => {
+		console.log("value:", value);
+		setPassWord(value);
+	};
+	return (
+		<div className="flex justify-center items-center h-screen bg-blue-50">
+      <div className="flex flex-col w-full max-w-screen-lg px-4 bg-white rounded-2xl drop-shadow-xl">
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <form className="flex flex-col" >
+            <label for="userName" >User Name</label>
+              <input type="text" name="userName" onChange={(e) => handleChangeUserName(e.target.value)}/>
+            <label for="passWord" >Password</label>
+              <input type="text" name="passWord" onChange={(e) => handleChangePassWord(e.target.value)}/>
+            {/* <input type="submit" value="Submit" /> */}
+            <button type="submit" onClick={handleSubmit}>Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+	)
+}
